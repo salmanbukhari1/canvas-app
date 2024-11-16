9a1cca86402cef538cc731fa0d3b52d2bad72a13
salman <s.bukhari577@gmail.com>
2024-11-16 21:07:00 +1100

    Clean up

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -3,7 +3,6 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 
 admin.initializeApp();
-// const db = admin.firestore();
 
 const checkAuth = async (req, res, next) => {
   try {
