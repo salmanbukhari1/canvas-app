2ab920f6646195928007501586d060c7dcbf71f2
salman <s.bukhari577@gmail.com>
2024-11-18 07:57:15 +1100

    Clean up

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,59 +1,23 @@
 // frontend/pages/_app.js
 import '../styles/globals.css';
-import { useEffect } from "react";
+import { Provider } from 'react-redux';
 import { store } from '../redux/store';
-import { auth } from "../lib/firebase";
-import Header from '../components/Layout/Header';
-import { onAuthStateChanged } from "firebase/auth";
-import { Provider, useDispatch } from 'react-redux';
-import Container from '../components/Layout/Container';
-import { setUser, clearUser, setLoading } from "../redux/authSlice";
-
-function AuthProvider({ children }) {
-    const dispatch = useDispatch();
-  
-    useEffect(() => {
-      const unsubscribe = onAuthStateChanged(auth, async (user) => {
-        if (user) {
-            const serializedUser = {
-                uid: user.uid,
-                email: user.email,
-                displayName: user.displayName,
-                photoURL: user.photoURL,
-            };
-
-            // If the user is logged in, get the Firebase ID token
-            const token = await user.getIdToken();
-
-            // Set token as a cookie so it can be sent with every server-side request
-            document.cookie = `token=${token}; path=/`;
-
-            dispatch(setUser(serializedUser)); // Update Redux with the authenticated user
-
-        } else {
-            // If the user is not logged in, delete the token cookie
-            document.cookie = 'token=; path=/; max-age=0';
-            dispatch(clearUser(null)); // Clear user from Redux when logged out
-        }
-        dispatch(setLoading(false));
-      });
-  
-      // Clean up the listener on component unmount
-      return () => unsubscribe();
-    }, [dispatch]);
-  }
+import Header from '../components/Layout/header';
+import AuthProvider from '../components/Auth/authProvider';
+import Container from '../components/Layout/container';
 
 function MyApp({ Component, pageProps }) {
 
-  return (
+    return (
       <Provider store={store}>
-          <Header />
-          <AuthProvider />
-          <Container>
-            <Component {...pageProps} />
-          </Container>
+        <Header />
+          <AuthProvider>
+            <Container>
+                <Component {...pageProps} />
+            </Container>
+          </AuthProvider>
       </Provider>
-  );
+    );
 }
 
 export default MyApp;
